e467b113fa42569d1556c42ee3e0449159ca208e
odddyyy <56987696+odddyyy@users.noreply.github.com>
2020-03-11 11:09:16 +0700

    refreshing server

--- client/script.js
+++ client/script.js
@@ -1,6 +1,38 @@
 const END_POINT = `https://protected-crag-71554.herokuapp.com`
 
-const navBar = $('#nav-bar')
-const home = $('#home')
+const loginPassword = $('#pass-login')
+const loginEmail = $('#email-login')
+const email = $('#email')
+var token = localStorage.getItem('access_token')
+console.log(token)
 
-home.hide()
+$(document).ready(() => {
+    if (token) {
+
+    } else {
+
+    }
+})
+
+
+$('#login-form').on('submit', (event) => {
+    event.preventDefault()
+    $.ajax({
+        method:'POST',
+        url: `https://protected-crag-71554.herokuapp.com/users/login`,
+        data: {
+            email:loginEmail.val(),
+            password:loginPassword.val()
+        },
+        success: (data) => {
+            console.log(data)
+            localStorage.setItem('access_token',data.access_token)
+        },
+        error: (response) => {
+            $('#err-alert-login').text(response.responseJSON)
+        }
+    })
+    
+})
+
+$('#login-page').show()
